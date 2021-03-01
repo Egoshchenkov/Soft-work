0af711435595e65f7f37490ae3ee56ebf8232255
Egoshchenkov <IlyaEgoshchenkov>
2021-03-01 20:50:59 +0300

    верстка плюс гет пост

---
+++ index.js
@@ -0,0 +1,19 @@
+// fetch('http://localhost:3000/posts').then(function(response) {
+//     return response.text();
+// }).then(function(text) {
+//     alert(text);
+// });
+
+// fetch('http://localhost:3000/posts',{
+//     method: "POST",
+//     headers: {
+    
+//       'Content-Type': 'application/json',
+//     },
+//     body:  JSON.stringify({
+//       x:'1',
+//     }),
+//  })
+ 
+
+
